7774149afc20cc5231c7dc9f6defe450e5032264
Jonathan East <jonathan_east@ymail.com>
2020-07-06 15:42:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,6 +61,17 @@ class Student
     Student.new(id, name, grade)
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+      SQL
+
+      DB[:conn].execute(sql, name).map { |row| self.new_from_db(row)  }.first
+  end
+
   def update
     sql = <<-SQL
       UPDATE students
